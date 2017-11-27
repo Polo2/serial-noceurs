9e5d5d75708f1e12b436cc95177cc5e58eec3448
Aurelie Dubuis <aurel.dubs@gmail.com>
2017-11-27 18:24:42 +0100

    Wedding model routes and weddings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+  get 'weddings/index'
+
+  get 'weddings/show'
+
+  get 'weddings/new'
+
+  get 'weddings/create'
+
+  get 'weddings/edit'
+
+  get 'weddings/update'
+
+  get 'weddings/destroy'
+
   devise_for :users
+  resources :weddings
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
